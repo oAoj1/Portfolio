42f63ffd627662d234cc5af76868d1eb6dfaf73d
oAoj1 <joaogbmarques@gmail.com>
2022-11-27 22:34:41 -0300

    adicionando soft skills

--- src/Components/Section/index.jsx
+++ src/Components/Section/index.jsx
@@ -6,6 +6,11 @@ import { DiCss3 } from 'react-icons/di'
 import { IoLogoNodejs } from 'react-icons/io'
 import { FaPython } from 'react-icons/fa'
 import { SiTypescript } from 'react-icons/si'
+import { GiBrain } from 'react-icons/gi'
+import { BiHappyBeaming } from 'react-icons/bi'
+import { AiFillHeart } from 'react-icons/ai'
+import { FaHandRock } from 'react-icons/fa'
+import { GiThink } from 'react-icons/gi'
 
 export default function Section(){
 
@@ -25,25 +30,43 @@ export default function Section(){
                 </div>
 
                 <div className="skillContent">
-                    <h2>Habilidades</h2>
-                    <p>Veja tecnologias em que possuo um <span className='verde'>breve conhecimento</span>, outras que <span className='amarelo'>estou estudando</span> e algumas que também já tive contato <span className='vermelho'>poucas vezes</span> </p>
-                    <div className="techContent">
-                        <ul>
-                            <li><AiFillHtml5/></li>
-                            <li><DiCss3/></li>
-                            <li><DiJavascript1/></li>
-                            <li><FaReact/></li>
-                            <li><IoLogoNodejs/></li>
-                            <li><FaPython/></li>
-                            <li><SiTypescript/></li>
-                        </ul>
+
+                    <div className="hardSkillsContent">
+                        <h2> <i>Hard Skills</i> </h2>
+                        <p>Veja tecnologias em que possuo um <span className='verde'>breve conhecimento</span>, outras que <span className='amarelo'>estou estudando</span> e algumas que também já tive contato <span className='vermelho'>poucas vezes</span> </p>
+
+                        <div className="techContent">
+                            <ul>
+                                <li>hmtl<AiFillHtml5/></li>
+                                <li>css<DiCss3/></li>
+                                <li>java script<DiJavascript1/></li>
+                                <li>react.js<FaReact/></li>
+                                <li>node.js<IoLogoNodejs/></li>
+                                <li>python<FaPython/></li>
+                                <li>type script<SiTypescript/></li>
+                            </ul>
+                        </div>
+
+                        <div className="levelContent">
+                            <h4>Sublinhados de: </h4>
+                            <ul>
+                                <li> <span className='verde'>Verde:</span> Possuo um breve conhecimento da tecnologia</li>
+                                <li> <span className='amarelo'>Amarelo:</span> Estou estudando a tecnologia no momento</li>
+                                <li> <span className='vermelho'>Vermelho:</span>  Já tive um contato com a tecnologia, porém pouca expêriencia</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="levelContent">
-                        <h4>Sublinhados de: </h4>
+                    
+                    <hr className='linha'/>
+
+                    <div className="softSkillsContent">
+                        <h2> <i>Soft Skills</i> </h2>
                         <ul>
-                            <li> <span className='verde'>Verde:</span> Possuo um breve conhecimento da tecnologia</li>
-                            <li> <span className='amarelo'>Amarelo:</span> Estou estudando a tecnologia no momento</li>
-                            <li> <span className='vermelho'>Vermelho:</span>  Já tive um contato com a tecnologia</li>
+                            <li><GiBrain/>criatividade</li>
+                            <li><BiHappyBeaming/>carisma</li>
+                            <li><AiFillHeart/>empatia</li>
+                            <li><FaHandRock/>dedicação</li>
+                            <li><GiThink/>curiosidade</li>
                         </ul>
                     </div>
                 </div>
