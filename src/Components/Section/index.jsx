d6a927836503ad227de1f3984cfd78f69b3dc45d
oAoj1 <joaogbmarques@gmail.com>
2022-11-28 12:29:15 -0300

    adicionando projetos

--- src/Components/Section/index.jsx
+++ src/Components/Section/index.jsx
@@ -79,9 +79,62 @@ export default function Section(){
             </div>
 
             <div className="projectsContent">
+
                 <div className="myProjectsContent">
                     <h2>Meus projetos</h2>
                     <p>Enquanto estudava as tecnologias fui colocando meu aprendizado em prática e criando projetos, confira alguns:</p>
+
+                    <div className="subContentProjects">
+
+                        <div>
+                        <div className="innerProjectsContent">
+                            <img src="/imagens/esgotos.png" alt="esgotos" />
+                            <h3>Esgotos</h3>
+                            <span>Saiba como funciona os tratamentos de esgotos</span>
+                            <a target="_blank" href="https://esgotos.vercel.app">Clique aqui</a>
+                        </div>
+
+                        <div className="innerProjectsContent">
+                            <img src="/imagens/pesquisa.png" alt="buscar cep" />
+                            <h3>Buscar CEP</h3>
+                            <span>Encontre a sua localidade colocando seu CEP</span>
+                            <a target="_blank" href="https://buscar-cep-kappa.vercel.app">Clique aqui</a>
+                        </div>
+
+                        <div className="innerProjectsContent">
+                            <img src="/imagens/cortico.png" alt="cortico" />
+                            <h3>Cortiço</h3>
+                            <span>O Cortiço, uma das maiores obras de Aluíso de Azevedo</span>
+                            <a target="_blank" href="https://cortico.vercel.app">Clique aqui</a>
+                        </div>
+                        </div>
+                        
+                        <div>
+                        <div className="innerProjectsContent">
+                            <img src="/imagens/dietaa.png" alt="dieta" />
+                            <h3>Dieta</h3>
+                            <span>Calcule seu gasto calorico diário, basal e outra coisas</span>
+                            <a target="_blank" href="https://dieta-nu.vercel.app">Clique aqui</a>
+                        </div>
+
+                        <div className="innerProjectsContent">
+                            <img src="/imagens/imc1.png" alt="cortico" />
+                            <h3>IMC V. 1.0</h3>
+                            <span>Calcule seu Indice de Massa Corporal</span>
+                            <a target="_blank" href="https://imc-fhcq.vercel.app">Clique aqui</a>
+                        </div>
+
+                        <div className="innerProjectsContent">
+                            <img src="/imagens/imc2.png" alt="cortico" />
+                            <h3>IMC V. 1.1</h3>
+                            <span>Calcule seu Indice de Massa Corporal</span>
+                            <a target="_blank" href="https://indice-massa-corporal.vercel.app">Clique aqui</a>
+                        </div> 
+                        </div>
+                              
+
+                    </div>
+
                 </div>
             </div>
             
