130210b04c6f5d45d3994a5560464d4ab31124ba
oAoj1 <joaogbmarques@gmail.com>
2022-12-01 10:58:30 -0300

    tirando filtro

--- src/Components/Section/index.jsx
+++ src/Components/Section/index.jsx
@@ -19,9 +19,11 @@ import { GiThink } from 'react-icons/gi'
 import { MdSchool } from 'react-icons/md'
 import { BsClockFill } from 'react-icons/bs'
 import { RiGitRepositoryFill } from 'react-icons/ri'
+import { useState } from 'react'
 
 
 export default function Section(){
+    
     return(
         <section>
             <div className="infoContent">
@@ -82,119 +84,120 @@ export default function Section(){
                     <h2>Meus projetos</h2>
                     <p>Enquanto estudava as tecnologias fui colocando meu aprendizado em prática e criando projetos, confira alguns:</p>
 
-                    <div className="filterContent">
-                        <h4>Filtre os projetos:</h4>
-                        <ul>
-                            <li>todos</li>
-                            <li><FaReact title='React'/></li>
-                            <li><GiBrain title='Cursos'/></li>
-                            <li><RiGitRepositoryFill title='Repositórios'/></li>
-                        </ul>
-                    </div>
-
                     <div className="subContentProjects">
 
                         <hr className='linha'/>
 
-                        <div className="reactTitle">
-                            <h2>React</h2><FaReact/>
+                        <div className="reactContent">
+
+                            <div className="reactTitle">
+                                <h2>React</h2><FaReact/>
+                            </div>
+
+                            <div className="contentOne">
+                                <div className="innerProjectsContent">
+                                    <img src="/imagens/esgotos.png" alt="esgotos" />
+                                    <h3>Esgotos</h3>
+                                    <span>Saiba como funciona os tratamentos de esgotos</span>
+                                    <a target="_blank" href="https://esgotos.vercel.app">Clique aqui</a>
+                                </div>
+
+                                <div className="innerProjectsContent">
+                                    <img src="/imagens/pesquisa.png" alt="buscar cep" />
+                                    <h3>Buscar CEP</h3>
+                                    <span>Encontre a sua localidade colocando seu CEP</span>
+                                    <a target="_blank" href="https://buscar-cep-kappa.vercel.app">Clique aqui</a>
+                                </div>
+
+                                <div className="innerProjectsContent">
+                                    <img src="/imagens/cortico.png" alt="cortico" />
+                                    <h3>Cortiço</h3>
+                                    <span>O Cortiço, uma das maiores obras de Aluíso de Azevedo</span>
+                                    <a target="_blank" href="https://cortico.vercel.app">Clique aqui</a>
+                                </div>
+                            </div>
+                        
+                            <div className="contentTwo">
+                                <div className="innerProjectsContent">
+                                    <img src="/imagens/dietaa.png" alt="dieta" />
+                                    <h3>Dieta</h3>
+                                    <span>Calcule seu gasto calorico diário, basal e outra coisas</span>
+                                    <a target="_blank" href="https://dieta-nu.vercel.app">Clique aqui</a>
+                                </div>
+
+                                <div className="innerProjectsContent">
+                                    <img src="/imagens/imc1.png" alt="cortico" />
+                                    <h3>IMC V. 1.0</h3>
+                                    <span>Calcule seu Indice de Massa Corporal</span>
+                                    <a target="_blank" href="https://imc-fhcq.vercel.app">Clique aqui</a>
+                                </div>
+
+                                <div className="innerProjectsContent">
+                                    <img src="/imagens/imc2.png" alt="cortico" />
+                                    <h3>IMC V. 1.1</h3>
+                                    <span>Calcule seu Indice de Massa Corporal</span>
+                                    <a target="_blank" href="https://indice-massa-corporal.vercel.app">Clique aqui</a>
+                                </div> 
+                            </div>
+
                         </div>
 
-                        <div className="contentOne">
-                            <div className="innerProjectsContent">
-                                <img src="/imagens/esgotos.png" alt="esgotos" />
-                                <h3>Esgotos</h3>
-                                <span>Saiba como funciona os tratamentos de esgotos</span>
-                                <a target="_blank" href="https://esgotos.vercel.app">Clique aqui</a>
+                        <hr className='linha'/>
+
+                        <div className="coursesContent">
+
+                            <div className="coursesTitle">
+                                <h2>Cursos</h2><GiBrain/>
                             </div>
 
-                            <div className="innerProjectsContent">
-                                <img src="/imagens/pesquisa.png" alt="buscar cep" />
-                                <h3>Buscar CEP</h3>
-                                <span>Encontre a sua localidade colocando seu CEP</span>
-                                <a target="_blank" href="https://buscar-cep-kappa.vercel.app">Clique aqui</a>
-                            </div>
+                            <div className="contentThree">
+                                <div className="innerProjectsContent">
+                                    <img src="/imagens/organo.png" alt="organo" />
+                                    <h3>Organograma</h3>
+                                    <span>Organize seu equipe com o organograma</span>
+                                    <a target="_blank" href="https://organograma-oaoj1.vercel.app">Clique aqui</a>
+                                </div>
 
-                            <div className="innerProjectsContent">
-                                <img src="/imagens/cortico.png" alt="cortico" />
-                                <h3>Cortiço</h3>
-                                <span>O Cortiço, uma das maiores obras de Aluíso de Azevedo</span>
-                                <a target="_blank" href="https://cortico.vercel.app">Clique aqui</a>
+                                <div className="innerProjectsContent">
+                                    <BsClockFill className='clockLogo'/>
+                                    <h3>Estudos</h3>
+                                    <span>Cronometre seu tempo de estudo</span>
+                                    <a target="_blank" href="https://alura-studies-oaoj1.vercel.app">Clique aqui</a>
+                                </div>
                             </div>
                         </div>
                         
-                        <div className="contentTwo">
-                            <div className="innerProjectsContent">
-                                <img src="/imagens/dietaa.png" alt="dieta" />
-                                <h3>Dieta</h3>
-                                <span>Calcule seu gasto calorico diário, basal e outra coisas</span>
-                                <a target="_blank" href="https://dieta-nu.vercel.app">Clique aqui</a>
-                            </div>
-
-                            <div className="innerProjectsContent">
-                                <img src="/imagens/imc1.png" alt="cortico" />
-                                <h3>IMC V. 1.0</h3>
-                                <span>Calcule seu Indice de Massa Corporal</span>
-                                <a target="_blank" href="https://imc-fhcq.vercel.app">Clique aqui</a>
-                            </div>
-
-                            <div className="innerProjectsContent">
-                                <img src="/imagens/imc2.png" alt="cortico" />
-                                <h3>IMC V. 1.1</h3>
-                                <span>Calcule seu Indice de Massa Corporal</span>
-                                <a target="_blank" href="https://indice-massa-corporal.vercel.app">Clique aqui</a>
-                            </div> 
-                        </div>
-
                         <hr className='linha'/>
+                        
+                        <div className="repositoriesContent">
 
-                        <div className="coursesTitle">
-                            <h2>Cursos</h2><GiBrain/>
-                        </div>
-
-                        <div className="contentThree">
-                            <div className="innerProjectsContent">
-                                <img src="/imagens/organo.png" alt="organo" />
-                                <h3>Organograma</h3>
-                                <span>Organize seu equipe com o organograma</span>
-                                <a target="_blank" href="https://organograma-oaoj1.vercel.app">Clique aqui</a>
+                            <div className="repositoriesTitle">
+                                <h2>Outros repositórios</h2><RiGitRepositoryFill/>
                             </div>
 
-                            <div className="innerProjectsContent">
-                                <BsClockFill className='clockLogo'/>
-                                <h3>Estudos</h3>
-                                <span>Cronometre seu tempo de estudo</span>
-                                <a target="_blank" href="https://alura-studies-oaoj1.vercel.app">Clique aqui</a>
+                            <div className="contentFour">
+
+                                <div className="innerProjectsContent">
+                                    <SiJavascript className='jsLogo'/>
+                                    <h3>Projetos JavaScript</h3>
+                                    <span>Projetos simples feitos com Java Script</span>
+                                    <a target="_blank" href="https://github.com/oAoj1/Projetos-JavaScript">Clique aqui</a>
+                                </div>
+
+                                <div className="innerProjectsContent">
+                                    <MdSchool className='schoolLogo'/>
+                                    <h3>Trabalhos de escola</h3>
+                                    <span>Trabalhos apresentados na escola feitos em site</span>
+                                    <a target="_blank" href="https://github.com/oAoj1/Trabalhos-de-escola">Clique aqui</a>
+                                </div>
+
                             </div>
                         </div>
-
-                        <hr className='linha'/>
-                                    
-                        <div className="repositoriesTitle">
-                            <h2>Outros repositórios</h2><RiGitRepositoryFill/>
-                        </div>
-
-                        <div className="contentFour">
-
-                            <div className="innerProjectsContent">
-                                <SiJavascript className='jsLogo'/>
-                                <h3>Projetos JavaScript</h3>
-                                <span>Projetos simples feitos com Java Script</span>
-                                <a target="_blank" href="https://github.com/oAoj1/Projetos-JavaScript">Clique aqui</a>
-                            </div>
-
-                            <div className="innerProjectsContent">
-                                <MdSchool className='schoolLogo'/>
-                                <h3>Trabalhos de escola</h3>
-                                <span>Trabalhos apresentados na escola feitos em site</span>
-                                <a target="_blank" href="https://github.com/oAoj1/Trabalhos-de-escola">Clique aqui</a>
-                            </div>
-
-                        </div>
-
+                    
                     </div>
 
                 </div>
+
             </div>
             
         </section>
