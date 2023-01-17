5067262ed94247849ed2edb7e4ce83b42d58a6f6
oAoj1 <joaogbmarques@gmail.com>
2023-01-17 10:10:02 -0300

    adicionando al food

---
+++ src/Components/Projetos/Cursos/AlFood/index.jsx
@@ -0,0 +1,12 @@
+import Modelo from '../../Modelo'
+
+export default function AlFood(){
+    return(
+        <Modelo
+            urlImg='/imagens/hamburguer.png'
+            titulo='AlFood'
+            subtitulo='Página de restaurantes'
+            urlProjeto='https://alfood-main-amber.vercel.app'
+        />
+    )
+}
--- src/Data/projetosCursosData.jsx
+++ src/Data/projetosCursosData.jsx
@@ -1,8 +1,13 @@
 import Organograma from "../Components/Projetos/Cursos/Organograma"
+import AlFood from "../Components/Projetos/Cursos/AlFood"
 
 export const projetosCursosData = [
     {
         name:'Organograma',
         project:<Organograma/>
+    },
+    {
+        name:'Alfood',
+        project:<AlFood/>
     }
 ]
